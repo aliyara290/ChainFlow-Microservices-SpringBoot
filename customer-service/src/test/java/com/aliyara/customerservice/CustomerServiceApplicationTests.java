f49dbf8724582370f0ba5231456fd5e3b79b8cd3
Ali Yara <ali.yara.fr@gmail.com>
2025-11-20 11:44:09 +0100

    activate test profile in *ServiceApplication

--- customer-service/src/test/java/com/aliyara/customerservice/CustomerServiceApplicationTests.java
+++ customer-service/src/test/java/com/aliyara/customerservice/CustomerServiceApplicationTests.java
@@ -1,15 +1,11 @@
 package com.aliyara.customerservice;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
-@TestPropertySource(properties = {
-        "eureka.client.enabled=false"
-})
+@ActiveProfiles("test")
 class CustomerServiceApplicationTests {
 
 	@Test
